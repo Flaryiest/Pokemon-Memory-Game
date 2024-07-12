54d51d329609540a85a960302c28ccfdc0cd5a5c
Flaryiest <ericmzuo1@gmail.com>
2024-07-12 15:54:08 -0600

    cleaned code and fixed cards not generating

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,11 @@ import { useState, useEffect, useRef } from 'react'
 import PokemonCard from './PokemonCard'
 import Scoreboard from './Scoreboard'
 function App() {
-  const [pokemonImageList, setPokemonImageList] = useState([])
   const clickedPokemon = useRef([])
+  const [currentScore, setCurrentScore] = useState(0)
   const [pokemonCards, setPokemonCards] = useState([])
-  const [playing, setPlaying] = useState(false)
+
   
-  useEffect(() => {
-    let pokemonIds = getIds(12)
-    getPokemonImages(pokemonIds)
-    
-  }, [playing])
-
-  useEffect(() => {
-    let pokemonCardsTemp = []
-    for (let pokemon = 0; pokemon < pokemonImageList.length; pokemon++) {
-      pokemonCardsTemp.push(<PokemonCard image={pokemonImageList[pokemon]} id={pokemon} key={pokemon} handleClick={handleClick}></PokemonCard>)
-    }
-    setPokemonCards(pokemonCardsTemp)
-  }, [pokemonImageList])
-
   function getIds(number) {
     let currentIds = []
     while (currentIds.length < number) {
@@ -33,63 +19,73 @@ function App() {
   }
 
   async function getPokemonImages(pokemonIds) {
-    let currentImageList = structuredClone(pokemonImageList)
+    let currentImageList = []
     for (let id = 0; id < pokemonIds.length; id++) {
       const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonIds[id], {mode: "cors"})
       const pokemonFile = await pokemon.json()
       const pokemonImage = pokemonFile.sprites.front_default
       currentImageList.push(pokemonImage)    
     }
-
-    setPokemonImageList(currentImageList)
+    return currentImageList
   }
 
   function handleClick(event) {
     if (!(clickedPokemon.current.includes(event.target.id))) {
       clickedPokemon.current = clickedPokemon.current.concat([event.target.id])
+      let newScore = currentScore + 1
+      setCurrentScore(newScore)
+      console.log(currentScore)
       shuffleCards(pokemonCards)
-
     }
     else {
       console.log("You lost")
+      setCurrentScore(0)
     }
 
   }
+  function generatePokemonCards(pokemonImages) {
+    if (pokemonImages.length > 1) {
+      let pokemonCardsTemp = []
+      for (let pokemon = 0; pokemon < pokemonImages.length; pokemon++) {
+        pokemonCardsTemp.push({image:pokemonImages[pokemon], id:pokemon, key:pokemon })
+      }
+      setPokemonCards(pokemonCardsTemp)}
+  }
 
   function shuffleCards(pokemonCards) {
-    let currentPokemonCards = pokemonCards
+    let currentPokemonCards = structuredClone(pokemonCards)
     let currentIndex = currentPokemonCards.length
     while (currentIndex != 0) {
       let randomIndex = Math.floor(Math.random() * currentIndex)
       currentIndex--
       [currentPokemonCards[currentIndex], currentPokemonCards[randomIndex]] = [currentPokemonCards[randomIndex], currentPokemonCards[currentIndex]] 
     }
-    console.log(currentPokemonCards, "after shuffling")
-    let tempPokemonCards = []
-    for (let card = 0; card < currentPokemonCards.length; card++) {
-      tempPokemonCards.push(<PokemonCard image={currentPokemonCards[card].props.image} id={currentPokemonCards[card].props.id} key={currentPokemonCards[card].props.id} handleClick={handleClick}></PokemonCard>)
-    }
-    setPokemonCards(tempPokemonCards)
+
+    setPokemonCards(currentPokemonCards)
   }
 
-  function startGame() {
-    setPokemonImageList([])
-    setPokemonCards([])
-    setPlaying(true)
-    console.log(pokemonCards)
+  function createPokemonCards() {
+    let pokemonIds = getIds(12)
+    let pokemonImages = getPokemonImages(pokemonIds)
     
-    clickedPokemon.current = []
+    pokemonImages.then((response) => {
+      generatePokemonCards(response)
+    })
   }
-
-
+  function startGame() {
+    createPokemonCards()
+  }
   
   return (
     <>
       <Scoreboard currentClicked={clickedPokemon}></Scoreboard>
-      <button onClick={startGame}></button>
-      {pokemonCards}   
+      <button onClick={(startGame)}></button>
+      {pokemonCards.map((item) => {
+        return (<PokemonCard image={item.image} id={item.id} key={item.key} handleClick={handleClick}></PokemonCard>)
+      })} 
     </>
   )
 }
+
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react'
 
 function PokemonCard({image, id, handleClick}) {
-
-
-
     return <img src={image} id={id} onClick={handleClick}></img>
 }
 export default PokemonCard
